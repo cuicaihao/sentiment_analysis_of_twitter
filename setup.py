7928acbde9eb3555d8da7cd1486b7f9fd99450af
Chris Cui <caihao.cui@ieee.org>
2020-07-08 22:08:45 +1000

    init repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Perform Sentiment Analysis in Python 3 Using the Natural Language Toolkit (NLTK)',
+    author='chris cui',
+    license='MIT',
+)
